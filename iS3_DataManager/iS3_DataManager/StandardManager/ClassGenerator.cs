693b176a6779a0b0f0f84bb3602bf719e9dd2e57
flyong <2012790993@qq.com>
2019-04-17 15:23:19 +0800

    Update ClassGenerator.cs

--- iS3_DataManager/iS3_DataManager/StandardManager/ClassGenerator.cs
+++ iS3_DataManager/iS3_DataManager/StandardManager/ClassGenerator.cs
@@ -3,23 +3,39 @@ using Newtonsoft.Json;
 using iS3_DataManager.Models;
 using System;
 using System.Collections.Generic;
-
+using System.IO;
 
 namespace iS3_DataManager.StandardManager
 {
     public class ClassGenerator
     {
-        public void generateClass(DataSet ds)
+        public void generateClass( DomainDef domain)
         {try
             {
                 List<List<string>> list = new List<List<string>>();
-                foreach (DataTable dataTable in ds.Tables)
+                foreach (DGObjectDef dGObject in domain.DGObjectContainer)
                 {
-                    DataRow dataRow= dataTable.Rows[2];
-                    //for (int i = 0; i ; i++)
-                    //{
+                    List<string> property = new List<string>();
+                    List<string> type = new List<string>();
 
-                    //}
+                    foreach (PropertyMeta meta in dGObject.PropertyContainer)
+                    {
+                        property.Add(meta.PropertyName);
+                        type.Add(meta.DataType);
+                    }
+                    string newClass = @"using System; \n namespace iS3_DataManager.Models\n {\n public class "+dGObject.Code+"\n {";
+                    for(int i = 0; i < property.Count; i++)
+                    {
+                        newClass += "public " + type[i] + " " + property[i] + " {get;set;}\n";
+                    }
+                    newClass += "}\n}";
+                    string path = AppDomain.CurrentDomain.BaseDirectory + dGObject.Code + ".cs";
+                    FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                    StreamWriter sw = new StreamWriter(fs);
+                    sw.Write(newClass);
+                    sw.Flush();
+                    sw.Close();
+                    fs.Close();
                 }
                 
             }
@@ -29,5 +45,6 @@ namespace iS3_DataManager.StandardManager
                 
             }
         }
+        
     }
 }
